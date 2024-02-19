cb5de3f6ec631bdd7e78f7289a40aaa5608b285b
adguerou <adrien.guerou@univ-savoie.fr>
2024-02-19 17:29:05 +0100

    add function for ImageCollection monthly median

--- src/pyce/gee.py
+++ src/pyce/gee.py
@@ -1,7 +1,6 @@
 import dask
 import ee
 import geemap
-import geopandas as gpd
 import pandas as pd
 
 
@@ -113,3 +112,112 @@ def ee_to_gdf_by_slice(
         gdf.set_crs(crs, inplace=True)
 
     return gdf
+
+
+def set_month(image: ee.Image, first_day_of_month: int):
+    """
+    Return the month of an GEE image based on the first day given. Useful to filter
+    images between, for instance, the 15th of each month.
+
+    :param image: GEE image
+    :param first_day_of_month: First of the month to consider.
+           If 15 is given, images from the 14th will be tagged month-1
+    :return: Month of the image as int
+    """
+    if not 1 <= first_day_of_month <= 31:
+        raise ValueError(
+            f"first_day_of_month must be within the range [1,31], got {first_day_of_month}"
+        )
+    date = image.date()
+
+    month = ee.Algorithms.If(
+        date.get("day").gte(first_day_of_month),
+        date.get("month"),
+        date.get("month") - 1,
+    )
+
+    return month
+
+
+def ic_monthly_median(
+    ic: ee.ImageCollection,
+    month_list: list = None,
+    first_day_of_month=1,
+    month_prop_name: str = "MONTH",
+    rename_band: bool = True,
+    band_names: list[str] = None,
+):
+    # -------------------------------------------------------------------------------
+    def _get_monthly_median(
+        ic: ee.ImageCollection,
+        month: int = None,
+        month_prop_name: str = "MONTH",
+    ):
+        """
+        Select images from a given month within an ImageCollection, return its median
+        :param ic:
+        :param month:
+        :param month_prop_name:
+        :return:
+        """
+
+        return (
+            ic.filter(ee.Filter.eq(month_prop_name, month))
+            .median()
+            .set(month_prop_name, month)
+        )
+
+    def _create_band_names(bands_list: list[str] = None, month_list: list[int] = None):
+        """
+        Return a new band list to rename ImageCollection with the month indicated per band.
+        This is  usefull to transform imageCollection to single Imge with multiple bands.
+
+        :param bands_list:
+        :param month_list:
+        :return:
+        """
+        new_band_names = []
+
+        # For loop in this order to be consistent with the flattening of bands when transforming
+        # an ImageCollection to single Image with bands. First is the month as one image in the
+        # ImageCollection corresponds to each month
+
+        for month in month_list:
+            for band in bands_list:
+                new_band_names.append(f"{band}_{month}")
+        return new_band_names
+
+    # -------------------------------------------------------------------------------
+
+    # =============
+    # Function start
+    # =============
+
+    # Add a tagg to each image according to its month belonging
+    ic_month_tagged = ic.map(
+        lambda img: img.set(
+            month_prop_name,
+            set_month(img, first_day_of_month=first_day_of_month),
+        )
+    )
+
+    # Get the monthly median
+    monthly_median_coll = ee.ImageCollection.fromImages(
+        ee.List(month_list).map(
+            lambda month: _get_monthly_median(
+                ic_month_tagged,
+                month,
+                month_prop_name=month_prop_name,
+            )
+        )
+    )
+
+    # Transform the ImageCollection to an image with renamed bands
+    if rename_band:
+        new_band_names = _create_band_names(
+            bands_list=band_names, month_list=month_list
+        )
+
+        return monthly_median_coll.toBands().rename(new_band_names)
+    else:
+        return monthly_median_coll
