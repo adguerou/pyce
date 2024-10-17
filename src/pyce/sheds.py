bffa4394d53ed611549502f5811e65c3fe87804e
adguerou <adrien.guerou@univ-savoie.fr>
2024-10-17 17:51:03 +0200

    split run_pyshed function to more modular functions + create dask function of the main functions

--- src/pyce/sheds.py
+++ src/pyce/sheds.py
@@ -4,59 +4,22 @@ from typing import Union
 import dask
 import geopandas as gpd
 import numpy as np
-import pandas as pd
 import pyproj
 import rioxarray as rioxr
-import shapely
 import xarray as xr
 from pysheds.grid import Grid
 from pysheds.view import Raster, ViewFinder
-from rioxarray import merge
-from shapely.geometry import MultiPolygon, Point, Polygon, shape
+from shapely.geometry import MultiPolygon, Point, shape
 from shapely.geometry.base import BaseGeometry
 
-from pyce import shape as pyshape
+from pyce.raster import polygonize_raster
+from pyce.shape import fill_geom, select_poly_from_multipoly
 
 
 # =====================================
 #       General functions
 # =====================================
-def merge_rioxr(
-    mnt_list: Union[list, str] = None, epsg: str = None, save_name: str = None
-):
-    """
-    Merge rasters with rioxarray
-
-    :param mnt_list: List of files to merge.
-    :param epsg: EPSG code to project the tiles to before merging in case the file
-                 format do not contain this information. It must be the same for
-                 all files.
-    :param save_name: Full path to save merged raster. Default None (no savings)
-
-    :return: A rioxarray rasters of the merged tiles
-    """
-    rasters = []
-
-    if isinstance(mnt_list, str):
-        mnt_list = [mnt_list]
-
-    for mnt in mnt_list:
-        rst = rioxr.open_rasterio(mnt, mask_and_scale=True)
-        if epsg is not None:
-            rst = rst.rio.write_crs(epsg)
-        rasters.append(rst)
-
-    if len(rasters) == 1:
-        return rasters[0]
-
-    merged_raster = merge.merge_arrays(rasters)
-
-    if save_name is not None:
-        merged_raster.rio.to_raster(save_name)
-
-    return merged_raster
-
-
+# TODO: change name and update usage
 def shape_to_gdf(site_and_shapes, crs=None):
     """
     Transform dask outputs of run_pyshed to a GeoDataframe containing
@@ -124,9 +87,205 @@ def raster_shed_from_rioxr(raster_rioxr: rioxr.raster_array, band=0) -> Raster:
     )
 
 
+def raster_shed_to_geom(
+    grid: Grid, raster: Raster, as_geom=True, dtype: type = np.uint8
+) -> Union[tuple[dict, float], BaseGeometry]:
+    """
+    Transform catchment from Pyshed to a Multipolygon shape
+    :param grid: Grid object from pyshed
+    :param raster: Raster object from pyshed
+    :param as_geom: Default True, return a BaseGeometry,
+           If False, return a list of tuple (geojson dict, value)
+    :return: BaseGeometry, except if 'as_polygon' is False
+             then return a list of tuple (geojson dict, value)
+    """
+    # Clip to catchment
+    grid.clip_to(raster)
+
+    # Create view
+    catch_view = grid.view(raster, dtype=dtype)
+
+    # Create a vector representation of the catchment mask
+    # It is a list of tuple (geojson dict, value)
+    geom = [*grid.polygonize(catch_view)]
+
+    # Transform geojson dict to shapely Polygon/Multipolygon
+    if as_geom:
+        polys = list(map(lambda x: x[0], geom))
+        if len(polys) > 1:
+            geom = MultiPolygon([shape(p) for p in polys])
+        else:
+            geom = shape(polys[0])
+
+    return geom
+
+
+def raster_shed_to_rioxr(raster: Raster, crs=None, dtype="float32") -> xr.DataArray:
+    return (
+        xr.DataArray(
+            raster,
+            dims=["y", "x"],
+            coords={
+                "y": raster.coords[:, 0].reshape(raster.shape)[:, 0],
+                "x": raster.coords[:, 1].reshape(raster.shape)[0],
+            },
+        )
+        .astype(dtype)
+        .rio.write_nodata(raster.nodata)
+        .rio.write_transform(raster.affine)
+        .rio.write_crs(crs)
+    )
+
+
 # =====================================
 #       pyshed running functions
 # =====================================
+def get_lake_shape(
+    dem: Union[str, xr.DataArray],
+    xy_lake,
+    flattening_thresh=0.3,
+    buffer_size: float = 8,
+    bufffer_delta: float = 1.5,
+    crs_dem: str = None,
+    crs_lake: str = None,
+    as_gdf=False,
+    name_lake: str = "lake",
+    save_name: str = None,
+):
+    # Get dem
+    # =======
+    if isinstance(dem, str):
+        dem = rioxr.open_rasterio(dem, mask_and_scale=True)
+    if not isinstance(dem, xr.DataArray):
+        raise TypeError(f"'dem' type must either str or xr.DataArray. Got {type(dem)}")
+
+    # Check projection
+    # ================
+    if crs_dem is None:
+        crs_dem = dem.rio.crs
+
+    elif crs_dem != dem.rio.crs:
+        raise ValueError(
+            f"'dem' has a different projection ({dem.rio.crs}) than 'crs_dem'({crs_dem})"
+        )
+
+    if crs_lake is None:
+        crs_lake = crs_dem
+        print(
+            Warning(f"EPSG_LAKE is not set, it will be assumed to be equal to EPSG_DEM")
+        )
+
+    # Get lake coordinates into dem coordinates
+    # ========================================
+    xy_lake = (
+        gpd.GeoSeries.from_xy([xy_lake[0]], [xy_lake[1]], crs=crs_lake)
+        .to_crs(crs_dem)
+        .get_coordinates()
+    )
+    xy_lake = [xy_lake["x"].values[0], xy_lake["y"].values[0]]
+
+    # Lake selection
+    # ==============
+    altitude_lake = dem.sel(
+        x=xy_lake[0],
+        y=xy_lake[1],
+        method="nearest",
+    ).values[0]
+
+    if np.isnan(altitude_lake):
+        raise ValueError(
+            f"'xy_lake' fell onto NaN value of dem. Please verify projections and/or dem data"
+        )
+
+    lake_sel = np.abs(dem.data - altitude_lake) < flattening_thresh
+    dem_lake = dem.where(lake_sel)
+
+    # Polygonize
+    # ==========
+    lake_shp = polygonize_raster(
+        data=dem_lake.isnull(),
+        mask=~dem_lake.isnull(),
+        connectivity=4,
+        transform=dem_lake.rio.transform(),
+    )
+
+    # Fill geometries
+    if buffer_size is not None:
+        lake_shp = fill_geom(
+            lake_shp, buffer_size=buffer_size, buffer_delta=bufffer_delta
+        )
+
+    # Select geometry overlapping lake coordinates
+    lake_shp = select_poly_from_multipoly(lake_shp, selection=Point(xy_lake))
+
+    # Transform to geodataframe + save if asked
+    # ================================
+    lake_shp_gdf = gpd.GeoDataFrame(
+        {"lake": [name_lake]}, geometry=[lake_shp], crs=crs_lake
+    )
+    if save_name is not None:
+        lake_shp_gdf.to_file(save_name)
+
+    if as_gdf:
+        lake_shp = lake_shp_gdf
+
+    return lake_shp
+
+
+def burn_lake_to_dem(
+    dem: xr.DataArray,
+    lake_shp: Union[BaseGeometry, gpd.GeoDataFrame],
+    crs_lake: str = None,
+    method: Union[str, float] = "min",
+    save_name: str = None,
+):
+    # Check type and get correct geometry
+    if not isinstance(lake_shp, Union[gpd.GeoDataFrame, BaseGeometry]):
+        raise TypeError(
+            f"'lake_shp' must be part of  Union[BaseGeometry, gpd.GeoDataFrame],"
+            f" got {type(lake_shp)}"
+        )
+
+    # Check projections
+    if isinstance(lake_shp, BaseGeometry):
+        if crs_lake is None:
+            crs_lake = dem.rio.crs
+            print(
+                Warning(
+                    f"CRS_LAKE is not set, it will be assumed to be equal to CRS_DEM"
+                )
+            )
+        lake_shp = gpd.GeoDataFrame(
+            {"lake": ["lake"]}, geometry=[lake_shp], crs=crs_lake
+        )
+
+    # Reproject to same coordinates + selection of lake
+    lake_shp = lake_shp.to_crs(dem.rio.crs)
+    lake_sel = ~dem.rio.clip(lake_shp.geometry, all_touched=True, drop=False).isnull()
+
+    # Get value to replace lake value with / Min by default
+    allowed_meth = ["min", "max", "mean", "median"]
+    lake_alt = dem.where(lake_sel).min().data
+
+    if isinstance(method, Union[int, float]):
+        lake_alt = method
+    elif not any([method == meth for meth in allowed_meth]):
+        raise ValueError(f"'method' must be part of {allowed_meth}, got {method}")
+    elif method == "max":
+        lake_alt = dem.where(lake_sel).max().data
+    elif method == "mean":
+        lake_alt = dem.where(lake_sel).mean().data
+    elif method == "median":
+        lake_alt = dem.where(lake_sel).median().data
+
+    # Burn dem
+    dem_burnt = dem.where(~lake_sel, other=lake_alt)
+
+    if save_name is not None:
+        dem_burnt.rio.to_raster(save_name)
+    return dem_burnt
+
+
 def raster_shed_processing(raster_shed: Raster):
     """
     Process a raster shed by filling pits, depressions and flats,
@@ -189,180 +348,36 @@ def get_catchment(
     return [catch, x_snap, y_snap, acc]
 
 
-def raster_shed_to_geom(
-    grid: Grid, raster: Raster, as_geom=True, dtype: type = np.uint8
-) -> Union[tuple[dict, float], BaseGeometry]:
-    """
-    Transform catchment from Pyshed to a Multipolygon shape
-    :param grid: Grid object from pyshed
-    :param raster: Raster object from pyshed
-    :param as_geom: Default True, return a BaseGeometry,
-           If False, return a list of tuple (geojson dict, value)
-    :return: BaseGeometry, except if 'as_polygon' is False
-             then return a list of tuple (geojson dict, value)
-    """
-    # Clip to catchment
-    grid.clip_to(raster)
-
-    # Create view
-    catch_view = grid.view(raster, dtype=dtype)
-
-    # Create a vector representation of the catchment mask
-    # It is a list of tuple (geojson dict, value)
-    geom = [*grid.polygonize(catch_view)]
-
-    # Transform geojson dict to shapely Polygon/Multipolygon
-    if as_geom:
-        polys = list(map(lambda x: x[0], geom))
-        if len(polys) > 1:
-            geom = MultiPolygon([shape(p) for p in polys])
-        else:
-            geom = shape(polys[0])
-
-    return geom
-
-
-def raster_shed_to_rioxr(raster: Raster, crs=None, dtype="float32") -> xr.DataArray:
-    return (
-        xr.DataArray(
-            raster,
-            dims=["y", "x"],
-            coords={
-                "y": raster.coords[:, 0].reshape(raster.shape)[:, 0],
-                "x": raster.coords[:, 1].reshape(raster.shape)[0],
-            },
-        )
-        .astype(dtype)
-        .rio.write_nodata(raster.nodata)
-        .rio.write_transform(raster.affine)
-        .rio.write_crs(crs)
-    )
-
-
-@dask.delayed
-def run_pyshed(
-    name: str,
-    mnt: Union[str, list, pd.DataFrame],
+def get_shed(
+    dem: xr.DataArray,
     xy_outlet: list = None,
-    xy_center: list = None,
-    df_metadata: dict = None,
-    epsg_mnt: str = None,
-    epsg_lake: str = None,
-    save_mnt_dir: str = None,
-    dem_lake_flattening: bool = True,
-    flattening_threshold: float = 1,
+    crs_outlet: str = None,
+    name_shed: str = "shed",
+    buffer_size=5,
+    buffer_delta=0,
+    save_name: str = None,
 ):
-    # Select /create mnt raster
-    # =========================
-    if isinstance(mnt, str) or isinstance(mnt, list):
-        try:
-            dem_rioxr = merge_rioxr(mnt, epsg=epsg_mnt)
-        except Exception as e:
-            print(e)
-
-    # dataframe
-    elif isinstance(mnt, pd.DataFrame):
-        metadata_col = [
-            "col_name_dir",
-            "col_name_tile",
-            "col_name_x_center",
-            "col_name_y_center",
-            "col_name_x_outlet",
-            "col_name_y_outlet",
-            "tile_extension",
-        ]
-        if df_metadata is None:
-            df_metadata = dict(
-                col_name_dir="DIR_DALLE",
-                col_name_tile="NOM_DALLE",
-                col_name_x_center="XC",
-                col_name_y_center="YC",
-                col_name_x_outlet="X",
-                col_name_y_outlet="Y",
-                tile_extension=".asc",
-            )
-        # Check validity of dataframe columns vs metadata
-        if not all(pd.Series(df_metadata.keys()).isin(metadata_col)):
-            raise IOError(
-                f"df_metadata must contains the following keys: {metadata_col}"
-            )
-
-        for key, item in df_metadata.items():
-            if key.startswith("col"):
-                if not any(mnt.columns.isin([item])):
-                    raise IOError(f"mnt dataframe column '{item}' not found")
-
-        # Get list of tiles within dataframe
-        list_mnt = [
-            os.path.join(dir_tile, name_tile)
-            for dir_tile, name_tile in zip(
-                mnt[df_metadata["col_name_dir"]],
-                mnt[df_metadata["col_name_tile"]] + df_metadata["tile_extension"],
-            )
-        ]
-
-        # Merge the list of files
-        dem_rioxr = merge_rioxr(list_mnt, epsg=epsg_mnt)
-
-    # Get coordinates of the outlet and center of lake
-    # ================================================
-    if epsg_lake is None:  # make sure epsg are the same between mnt and outlet
+    # Check projection
+    # ================
+    crs_dem = dem.rio.crs
+    if crs_outlet is None:
+        crs_outlet = crs_dem
         print(
-            Warning(f"EPSG_SHED is not set, it will be assumed to be equal to EPSG_MNT")
+            Warning(f"CRS_OUTLET is not set, it will be assumed to be equal to CRS_DEM")
         )
-        epsg_lake = epsg_mnt
 
-    if xy_outlet is None:
-        xy_outlet = (
-            gpd.GeoSeries.from_xy(
-                mnt[df_metadata["col_name_x_outlet"]],
-                mnt[df_metadata["col_name_y_outlet"]],
-                crs=epsg_lake,
-            )
-            .to_crs(epsg_mnt)
-            .get_coordinates()
-        )
-        xy_outlet = [xy_outlet["x"].values[0], xy_outlet["y"].values[0]]
-
-    if xy_center is None:
-        xy_center = (
-            gpd.GeoSeries.from_xy(
-                mnt[df_metadata["col_name_x_center"]],
-                mnt[df_metadata["col_name_y_center"]],
-                crs=epsg_lake,
-            )
-            .to_crs(epsg_mnt)
-            .get_coordinates()
-        )
-        xy_center = [xy_center["x"].values[0], xy_center["y"].values[0]]
-
-    # Ensure that altitude of lake around outlet points are the same
-    # This aims flats to be real flats for shed delimitation
-    # Use a height threshold to flatten data points
-    # ===============================================================
-    if dem_lake_flattening:
-        altitude_lake = dem_rioxr.sel(
-            x=xy_center[0],
-            y=xy_center[1],
-            method="nearest",
-        ).values[0]
-
-        lake_sel = (dem_rioxr.data > altitude_lake - flattening_threshold) & (
-            dem_rioxr.data < altitude_lake + flattening_threshold
-        )
-        dem_rioxr = dem_rioxr.where(~lake_sel, other=dem_rioxr.where(lake_sel).min())
-
-        return dem_rioxr, lake_sel
-
-    # Save merged/ flattened MNT raster
-    # =================================
-    if save_mnt_dir is not None:
-        save_mnt_name = os.path.join(save_mnt_dir, f"{name}_merged.tif")
-        dem_rioxr.rio.to_raster(save_mnt_name)
+    # Get outlet coordinates into dem coordinates
+    # ===========================================
+    xy_outlet = (
+        gpd.GeoSeries.from_xy([xy_outlet[0]], [xy_outlet[1]], crs=crs_outlet)
+        .to_crs(crs_dem)
+        .get_coordinates()
+    )
+    xy_outlet = [xy_outlet["x"].values[0], xy_outlet["y"].values[0]]
 
     # Create the pyshed Raster Object from rioxarray dem
     # ==================================================
-    raster_shed = raster_shed_from_rioxr(raster_rioxr=dem_rioxr)
+    raster_shed = raster_shed_from_rioxr(raster_rioxr=dem)
 
     # Process the Raster object
     # =========================
@@ -377,79 +392,77 @@ def run_pyshed(
         fdir=fdir,
     )
 
-    # Get gdf of the shed with coordinates of the outlet
-    # ==================================================
     # Transform catchment to shape + clean interiors
     catch_geom = raster_shed_to_geom(grid=grid, raster=catch)
-    catch_geom_filled = pyshape.remove_interiors_geom(
-        catch_geom, buffer_size=10, buffer_delta=1
+    catch_geom_filled = fill_geom(
+        catch_geom, buffer_size=buffer_size, buffer_delta=buffer_delta
     )
-    catch_geom_sel = pyshape.select_poly_from_multipoly(
-        catch_geom_filled, selection="area"
-    )
+    catch_geom_sel = select_poly_from_multipoly(catch_geom_filled, selection="area")
 
     # Create gdf
+    # ==========
     gdf_shed = gpd.GeoDataFrame(
         {
-            "Name": [name],
+            "lake": [name_shed],
             "X outlet": [xy_outlet[0]],
             "Y outlet": [xy_outlet[1]],
             "X snap": x_snap,
             "Y snap": y_snap,
         },
         geometry=[catch_geom_sel],
-        crs=epsg_mnt,
+        crs=crs_dem,
     )  # Data and geom as list not to provide an index
 
-    # Get lake shape from the dem flattening + clip out lake from shed
-    # ================================================================
-    if dem_lake_flattening:
-        # Select lake rioxr and transform to pyshed raster
-        lake = raster_shed_from_rioxr(dem_rioxr.where(lake_sel))
+    # Save
+    if save_name is not None:
+        gdf_shed.to_file(save_name)
 
-        # Create a shape format
-        lake_geoms = raster_shed_to_geom(grid=grid, raster=lake, dtype=np.float32)
-        lake_geom_filled = pyshape.remove_interiors_geom(
-            lake_geoms, buffer_size=10, buffer_delta=1
-        )
+    return gdf_shed
 
-        try:
-            lake_geom_sel = pyshape.select_poly_from_multipoly(
-                lake_geom_filled, selection=Point(xy_center)
-            )
-        except:
-            lake_geom_sel = pyshape.select_poly_from_multipoly(
-                lake_geom_filled, selection="area"
-            )
-            print(
-                Warning(
-                    f"{name.upper()}: Lake geometry does not contain the center coordinates. "
-                    f"Return the largest area geometry."
-                )
-            )
 
-        # Create lake dataframe
-        gdf_lake = gpd.GeoDataFrame(
-            {
-                "Name": [name],
-                "X outlet": [xy_outlet[0]],
-                "Y outlet": [xy_outlet[1]],
-            },
-            geometry=[lake_geom_sel],
-            crs=epsg_mnt,
-        )
-        gdf_shed["geometry"] = shapely.difference(catch_geom_filled, lake_geom_filled)
+@dask.delayed
+def run_get_shed(
+    dem: xr.DataArray,
+    xy_outlet: list = None,
+    crs_outlet: str = None,
+    name_shed: str = "shed",
+    buffer_size=5,
+    buffer_delta=0,
+    save_name: str = None,
+):
+    return get_shed(
+        dem=dem,
+        xy_outlet=xy_outlet,
+        crs_outlet=crs_outlet,
+        name_shed=name_shed,
+        buffer_size=buffer_size,
+        buffer_delta=buffer_delta,
+        save_name=save_name,
+    )
 
-    else:
-        # Create lake dataframe with empty geometry
-        gdf_lake = gpd.GeoDataFrame(
-            {
-                "Name": [name],
-                "X outlet": [xy_outlet[0]],
-                "Y outlet": [xy_outlet[1]],
-            },
-            geometry=[None],
-            crs=epsg_mnt,
-        )
 
-    return gdf_shed, gdf_lake
+@dask.delayed
+def run_get_lake_shape(
+    dem: Union[str, xr.DataArray],
+    xy_lake,
+    flattening_thresh=0.3,
+    buffer_size: float = 8,
+    bufffer_delta: float = 1.5,
+    crs_dem: str = None,
+    crs_lake: str = None,
+    as_gdf=False,
+    name_lake: str = "lake",
+    save_name: str = None,
+):
+    return get_lake_shape(
+        dem=dem,
+        xy_lake=xy_lake,
+        flattening_thresh=flattening_thresh,
+        buffer_size=buffer_size,
+        bufffer_delta=bufffer_delta,
+        crs_dem=crs_dem,
+        crs_lake=crs_lake,
+        as_gdf=as_gdf,
+        name_lake=name_lake,
+        save_name=save_name,
+    )
